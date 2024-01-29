dac4c0e0d620f4049c2993468ecd7f7727aad7e6
Bharati Singh <88208046+SinghBharati@users.noreply.github.com>
2024-01-29 21:33:47 +0530

    Form Validation && useRef Hook

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,9 +1,23 @@
 import Header from "./Header";
-import {useState} from "react";
-
+import {useState, useRef} from "react";
+import {checkValidData} from "../utils/validate";
 function Login() {
     const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null)
 
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const handleButtonClick = () => {
+        // Validate Form Data
+        const message = checkValidData(email.current.value, password.current.value)
+        setErrorMessage(message);
+        console.log(message);
+        console.log(email.current.value);
+        console.log(password.current.value)
+
+
+    }
     const toggleSingInForm = () => {
         setIsSignInForm(!isSignInForm);
     }
@@ -16,13 +30,37 @@ function Login() {
                      alt="backgroung"/>
             </div>
 
-            <form className='absolute w-3/12 p-12 my-36 mx-auto right-0 left-0 rounded text-white bg-black bg-opacity-80'>
+            <form
+                className='absolute w-3/12 p-12 my-36 mx-auto right-0 left-0 rounded text-white bg-black bg-opacity-80'
+                onSubmit={(e) => e.preventDefault()}
+            >
                 <h1 className="py-4 font-bold text-3xl">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
                 {!isSignInForm &&  (<input type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700 rounded"/>)}
-                <input type="email" placeholder="Email" className="p-4 my-4 w-full bg-gray-700 rounded"/>
-                <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-700 rounded"/>
-                <button className="p-4 my-6 w-full bg-red-700 rounded">{isSignInForm ? "Sign In" : "Sign Up"}</button>
-                <p className="p-4 my-4 cursor-pointer" onClick={() => toggleSingInForm()}>{isSignInForm ? "New to Netflix? Sign up now" : "Already registered? Sign In"}</p>
+                <input
+                    className="p-4 my-4 w-full bg-gray-700 rounded"
+                    type="email"
+                    placeholder="Email Address"
+                    ref={email}
+                    />
+                <input
+                    className="p-4 my-4 w-full bg-gray-700 rounded"
+                    type="password"
+                    placeholder="Password"
+                    ref={password}
+                    />
+
+                <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+
+                <button
+                    className="p-4 my-6 w-full bg-red-700 rounded"
+                    onClick={() => handleButtonClick()}>
+                    {isSignInForm ? "Sign In" : "Sign Up"}
+                </button>
+                <p
+                    className="p-4 my-4 cursor-pointer"
+                    onClick={() => toggleSingInForm()}>
+                    {isSignInForm ? "New to Netflix? Sign up now" : "Already registered? Sign In"}
+                </p>
             </form>
         </div>
     );
