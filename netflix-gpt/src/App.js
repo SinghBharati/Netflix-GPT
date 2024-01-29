b153fdf1f68123641f3643ecd86ae633e6b05875
Bharati Singh <88208046+SinghBharati@users.noreply.github.com>
2024-01-29 16:04:24 +0530

    Configured Tailwind

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xl font-bold bg-green-200">Hello! </h1>
+    </div>
+  );
+}
+
+export default App;
